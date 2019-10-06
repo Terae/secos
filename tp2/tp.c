5eb4ef44f9b63670b4c357074a0be7fb0c4a7e71
Terae <Terae@protonmail.com>
2019-10-06 14:43:54 +0200

    [TP2] Breakpoint interruption handling

--- tp2/tp.c
+++ tp2/tp.c
@@ -1,9 +1,33 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
+#include <excp.h>
 #include <info.h>
+#include <intr.h>
 
 extern info_t *info;
+extern void resume_from_intr(void);
+
+void bp_handler(void) {
+    debug("db_handler called\n");
+    asm("leave; iret;");
+}
+
+void bp_trigger(void) {
+    asm ("int3;");
+    debug("inside bp_trigger\n");
+}
+
+void init_bp_handler(void) {
+    idt_reg_t idtr;
+    get_idtr(idtr);
+    debug("@IDT:\t0x%x\n", &idtr);
+
+    int_desc(&idtr.desc[BP_EXCP], gdt_krn_seg_sel(1), (unsigned int)&bp_handler);
+}
 
 void tp()
 {
+    init_bp_handler();
+    bp_trigger();
+    debug("END OF TP2\n");
 }
